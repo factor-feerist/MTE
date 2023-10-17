5fea3a9ca1ceba5c44383ab2fa07d15820a196a9
factor-feerist <erik.blodoex@gmail.com>
2023-10-17 21:05:32 +0500

    Fix server.py

--- server.py
+++ server.py
@@ -1,32 +1,15 @@
 import socket
-import time
-
-#виды команд:
-#user open "file"
-#user close
-#user save "new name"
-#user update
-#user delete range
-#user insert index text
-
-#Ждем пока пользователь откроет файл
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(('localhost', 3030))
-s.sendall('user open file'.encode('utf-8'))
-data = s.recv(1024)
-print(data.decode('utf-8'))
-time.sleep(2)
-s.close()
 
+s.bind(('localhost', 3030))
+s.listen(1)
 while True:
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    s.connect(('localhost', 3030))
-    #если с интерфейса идет какая-то команда, делаем ее,
-    #иначе отправляем запрос на обновление
-
-    #если close, то break
-    s.sendall('cmd'.encode('utf-8'))
-    data = s.recv(1024)
-    print(data.decode('utf-8'))
-    time.sleep(2)
-    s.close()
+    conn, addr = s.accept()
+    data = conn.recv(1024)
+    if not data:
+        continue
+    t = data.decode('utf-8')
+    print(t)
+    #если запрос на обновление, то отправляем текущую версию файла
+    #conn.sendall((t[:6] + ', слушаю вас').encode('utf-8'))
+conn.close()
